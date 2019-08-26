4b2e03d27677994ea0ebdad3c354831e85514563
夕舞八弦 <1677759063@qq.com>
2019-08-26 11:24:24 +0800

    require once

--- src/SpecialCDNPurgeLog.php
+++ src/SpecialCDNPurgeLog.php
@@ -1,5 +1,5 @@
 <?php
-require dirname(dirname(__FILE__)) . '/vendor/autoload.php';
+require_once(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
 use MediaWiki\MediaWikiServices;
 use AlibabaCloud\Client\AlibabaCloud;
 use AlibabaCloud\Client\Exception\ClientException;
